86ae5c8e887ef78867257132340cf66406519b8f
kondorriano <nicobito@gmail.com>
2017-06-23 13:23:21 +0200

    smooth rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,7 +16,7 @@ public class Movement : MonoBehaviour {
         float yVel = Input.GetAxis("Vertical");
         float xVel = Input.GetAxis("Horizontal");
         myRig.velocity = speed * (Vector3.forward * yVel + Vector3.right * xVel);
-       
-        transform.LookAt(transform.position + myRig.velocity.normalized);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(myRig.velocity.normalized),0.4f);
+        //transform.LookAt(transform.position + myRig.velocity.normalized);
     }
 }
