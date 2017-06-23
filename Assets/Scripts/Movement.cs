78c3815167538141052ec6e065ee0f6a8449b7c2
kondorriano <nicobito@gmail.com>
2017-06-23 13:03:24 +0200

    base project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody myRig;
+    public float speed = 5f;
+    // Use this for initialization
+	void Start () {
+        myRig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yVel = Input.GetAxis("Vertical");
+        float xVel = Input.GetAxis("Horizontal");
+        myRig.velocity = speed * (Vector3.forward * yVel + Vector3.right * xVel);
+       
+        transform.LookAt(transform.position + myRig.velocity.normalized);
+    }
+}
